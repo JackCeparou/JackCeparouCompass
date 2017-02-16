67a13b4db188ce9d5d6ceb8f883ed4d99f3195f3
JackCeparou <JackCeparou@users.noreply.github.com>
2017-02-16 14:51:59 +0100

    Configurators

---
+++ Customize/DefaultPluginsConfigurator.cs
@@ -0,0 +1,83 @@
+namespace Turbo.Plugins.JackCeparouCompass.Customize
+{
+    using Turbo.Plugins.Default;
+
+    public class DefaultPluginsConfigurator : BasePlugin
+    {
+        public DefaultPluginsConfigurator()
+        {
+            Enabled = true;
+        }
+
+        public override void Customize()
+        {
+            // I'M BRAVE ENOUGH
+            Hud.TogglePlugin<MonsterPackPlugin>(true);
+            Hud.TogglePlugin<EliteMonsterAffixPlugin>(false);
+            /////////////////////////////////////////////////
+
+            Hud.RunOnPlugin<InventoryAndStashPlugin>(plugin =>
+            {
+                plugin.NotGoodDisplayEnabled = true;
+                plugin.DefinitelyBadDisplayEnabled = true;
+                //plugin.LooksGoodDisplayEnabled = true;
+            });
+
+            Hud.RunOnPlugin<AttributeLabelListPlugin>(plugin =>
+            {
+                plugin.LabelList.WidthFunc = () => Hud.Window.Size.Height * 0.0630f;
+            });
+
+            Hud.RunOnPlugin<FeetBuffListPlugin>(plugin =>
+            {
+                plugin.BuffPainter.ShowTimeLeftNumbers = true;
+                plugin.BuffPainter.Opacity = 0.85f;
+
+                // Iron Skin
+                plugin.RuleCalculator.Rules.Add(new BuffRule(291804) { IconIndex = null, MinimumIconCount = 1, ShowTimeLeft = true, IconSizeMultiplier = 1.0f, });
+            });
+
+            Hud.RunOnPlugin<AttributeLabelListPlugin>(plugin =>
+            {
+                var index = 9; //0..9
+
+                if (index < plugin.LabelList.LabelDecorators.Count && index >= 0)
+                {
+                    plugin.LabelList.LabelDecorators[index].TextFunc = () => Hud.Game.Me.Stats.PickupRange.ToString("#");
+                    plugin.LabelList.LabelDecorators[index].HintFunc = () => "pickup radius";
+                }
+            });
+
+            Hud.RunOnPlugin<GlobePlugin>(plugin =>
+            {
+                plugin.RiftOrbDecorator.Add(new GroundCircleDecorator(Hud)
+                {
+                    Brush = Hud.Render.CreateBrush(255, 240, 120, 240, 3),
+                    Radius = 1.5f
+                });
+            });
+
+            //Hud.RunOnPlugin<OtherPlayersPlugin>(plugin =>
+            //{
+            //    plugin.DecoratorByClass[HeroClass.Barbarian].GetDecorators<MapLabelDecorator>().ForEach(d =>
+            //    {
+            //        d.LabelFont = Hud.Render.CreateFont("tahoma", 6f, 255, 255, 255, 0, false, false, 128, 0, 0, 0, true);
+            //        d.Up = true;
+            //    });
+            //    plugin.DecoratorByClass[HeroClass.Barbarian].GetDecorators<GroundLabelDecorator>().ForEach(d =>
+            //    {
+            //        d.BorderBrush = Hud.Render.CreateBrush(255, 255, 255, 0, 1);
+            //        d.TextFont = Hud.Render.CreateFont("tahoma", 6f, 255, 255, 255, 0, false, false, 128, 0, 0, 0, true);
+            //    });
+            //});
+        }
+
+        //public override void PaintWorld(WorldLayer layer)
+        //{
+        //    Hud.RunOnPlugin<OtherPlayersPlugin>(plugin =>
+        //    {
+        //        plugin.DecoratorByClass[Hud.Game.Me.HeroClassDefinition.HeroClass].Paint(layer, Hud.Game.Me, Hud.Game.Me.FloorCoordinate, Hud.Game.Me.BattleTagAbovePortrait);
+        //    });
+        //}
+    }
+}
---
+++ Customize/JackCeparouConfigurator.cs
@@ -0,0 +1,55 @@
+namespace Turbo.Plugins.JackCeparouCompass.Customize
+{
+    using Turbo.Plugins.Default;
+
+    public class JackCeparouConfigurator : BasePlugin
+    {
+        public JackCeparouConfigurator()
+        {
+            Enabled = true;
+        }
+
+        public override void Customize()
+        {
+            Hud.RunOnPlugin<DoorsPlugin>(plugin => plugin.ShowInTown = true);
+
+            Hud.RunOnPlugin<JackCeparouCompass.GoblinPlugin>(plugin =>
+            {
+                var radiusTransformator = new StandardPingRadiusTransformator(Hud, 333);
+
+                plugin.AllGoblinDecorators().ForEach(goblin =>
+                {
+                    goblin.GetDecorators<MapShapeDecorator>().ForEach(decorator =>
+                    {
+                        var painter = decorator.ShapePainter as CircleShapePainter;
+                        if (painter != null)
+                        {
+                            decorator.RadiusTransformator = radiusTransformator;
+                        }
+                    });
+                    goblin.GetDecorators<GroundCircleDecorator>().ForEach(decorator =>
+                    {
+                        decorator.RadiusTransformator = radiusTransformator;
+                    });
+                });
+            });
+
+            //Hud.RunOnPlugin<JackCeparouCompass.DoorsPlugin>(plugin =>
+            //{
+            //    plugin.BridgesDecorators.GetDecorators<MapShapeDecorator>().ForEach(decorator =>
+            //    {
+            //        decorator.Brush = Hud.Render.CreateBrush(240, 126, 13, 255, 1);
+            //    });
+            //});
+        }
+
+        //public override void PaintWorld(WorldLayer layer)
+        //{
+        //    Hud.RunOnPlugin<OtherPlayersPlugin>(plugin =>
+        //    {
+        //        plugin.DecoratorByClass[Hud.Game.Me.HeroClassDefinition.HeroClass].Paint(layer, Hud.Game.Me, Hud.Game.Me.FloorCoordinate, Hud.Game.Me.BattleTagAbovePortrait);
+        //    });
+        //}
+    }
+
+}
