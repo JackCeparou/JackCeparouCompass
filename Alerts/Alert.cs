8c4d3341b620c6de5974587cdc0df186690f4873
JackCeparou <JackCeparou@users.noreply.github.com>
2017-02-24 17:00:33 +0100

    cache default font

--- Alerts/Alert.cs
+++ Alerts/Alert.cs
@@ -32,6 +32,8 @@
         public uint TextSnoId { get; set; }
         public Func<uint, string> AlertTextFunc { get; set; }
 
+        private static IFont defaultFont;
+
         /// <summary>
         /// Initializes a new instance of the <see cref="Alert" /> class.
         /// </summary>
@@ -40,19 +42,22 @@
         /// <param name="text">The text.</param>
         public Alert(IController hud, HeroClass heroClass = HeroClass.None, string text = null)
         {
+            if (defaultFont == null)
+                defaultFont = Hud.Render.CreateFont("tahoma", 11, 255, 244, 30, 30, false, false, 242, 0, 0, 0, true);
+
             Hud = hud;
             Enabled = true;
             MessageFormat = "{0}";
 
             if (text == null)
-                AlertTextFunc = (id) => Hud.GetLocalizedName(id);
+                AlertTextFunc = (id) => Hud.GuessLocalizedName(id);
             else
                 AlertTextFunc = (id) => text;
 
             Rule = new AlertRule(hud, heroClass);
             Label = new TopLabelDecorator(Hud)
             {
-                TextFont = Hud.Render.CreateFont("tahoma", 11, 255, 244, 30, 30, false, false, 242, 0, 0, 0, true),
+                TextFont = defaultFont,
                 TextFunc = () => string.Format(CultureInfo.InvariantCulture, MessageFormat, AlertTextFunc.Invoke(TextSnoId)),
             };
         }
