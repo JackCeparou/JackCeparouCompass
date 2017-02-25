3e19eff74fa4777dae2f0ab7101b44dba01709ee
JackCeparou <JackCeparou@users.noreply.github.com>
2017-02-26 00:35:37 +0100

    add alert loot

--- Customize/JackCeparouConfigurator.cs
+++ Customize/JackCeparouConfigurator.cs
@@ -1,3 +1,9 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using Turbo.Plugins.Jack.Alerts;
+
 namespace Turbo.Plugins.Jack.Customize
 {
     using Turbo.Plugins.Jack.Actors;
@@ -14,6 +20,73 @@ namespace Turbo.Plugins.Jack.Customize
         {
             Hud.RunOnPlugin<DoorsPlugin>(plugin => plugin.ShowInTown = true);
 
+            var itemsIds = new HashSet<uint>() { 1844495708 };
+            Hud.RunOnPlugin<PlayerTopAlertListPlugin>(plugin =>
+            {
+                plugin.AlertList.Alerts.Add(new Alert(Hud)
+                {
+                    MessageFormat = "\uD83C\uDF81 {0} \uD83C\uDF81",//??
+                    AlertTextFunc = (id) =>
+                    {
+                        var items = Hud.Game.Items.Where(item => item.Location == ItemLocation.Floor && item.Unidentified/**/ && itemsIds.Contains(item.SnoItem.Sno));
+
+                        if (items.Count() > 1)
+                            return string.Join(", ", items.GroupBy(k => k.FullNameLocalized).Select(item => string.Format(CultureInfo.InvariantCulture, "{0} {1}", item.Count(), item.Key)));
+
+                        return items.Count() == 1 ? items.First().FullNameLocalized : string.Empty;
+                    },
+                    Rule =
+                    {
+                        ShowInTown = true,
+                        VisibleCondition = (controller) => controller.Game.Items.Any(item => item.Location == ItemLocation.Floor && item.Unidentified/**/ && itemsIds.Contains(item.SnoItem.Sno)),
+                    }
+                });
+            });
+
+            Hud.RunOnPlugin<MinimapLeftAlertListPlugin>(plugin =>
+            {
+                var ancientRank = -1;
+                plugin.AlertList.VerticalCenter = false;
+                plugin.AlertList.RatioSpacerY = 0;
+
+                plugin.AlertList.Alerts.Add(new Alert(Hud)
+                {
+                    MultiLine = true,
+                    LinesFunc = () =>
+                    {
+                        return Hud.Game.Items.Where(item => item.Location == ItemLocation.Floor && item.Unidentified && item.SetSno != uint.MaxValue && item.AncientRank > ancientRank).Select(item => string.Format(CultureInfo.InvariantCulture, "\uD83C\uDF81 {0} \uD83C\uDF81", item.FullNameLocalized));
+                    },
+                    Label =
+                    {
+                        TextFont = Hud.Render.CreateFont("tahoma", 7, 255, 0, 170, 0, false, false, true),
+                    },
+                    Rule =
+                    {
+                        ShowInTown = true,
+                        VisibleCondition = (controller) => controller.Game.Items.Any(item => item.Location == ItemLocation.Floor && item.Unidentified && item.SetSno != uint.MaxValue && item.AncientRank > ancientRank),
+                    }
+                });
+
+                plugin.AlertList.Alerts.Add(new Alert(Hud)
+                {
+                    MultiLine = true,
+                    LinesFunc = () =>
+                    {
+                        return Hud.Game.Items.Where(item => item.Location == ItemLocation.Floor && item.Unidentified && item.SetSno == uint.MaxValue && item.AncientRank > ancientRank).Select(item => string.Format(CultureInfo.InvariantCulture, "\uD83C\uDF81 {0} \uD83C\uDF81", item.FullNameLocalized));
+                    },
+                    Label =
+                    {
+                        TextFont = Hud.Render.CreateFont("tahoma", 7, 255, 235, 120, 0, false, false, true),
+                    },
+                    Rule =
+                    {
+                        ShowInTown = true,
+                        VisibleCondition = (controller) => controller.Game.Items.Any(item => item.Location == ItemLocation.Floor && item.Unidentified && item.SetSno == uint.MaxValue && item.AncientRank > ancientRank),
+                    }
+                });
+            });
+
+            #region GoblinPlugin
             /*
             Hud.RunOnPlugin<JackCeparouCompass.Monsters.GoblinPlugin>(plugin =>
             {
@@ -79,6 +152,7 @@ namespace Turbo.Plugins.Jack.Customize
                     LabelFont = Hud.Render.CreateFont("tahoma", 7, 180, 255, 163, 15, true, false, true)
                 });
             });/**/
+            #endregion
 
             //Hud.RunOnPlugin<JackCeparouCompass.DoorsPlugin>(plugin =>
             //{
