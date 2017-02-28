445c41b6b65d30abfceba6e8be0edda06205a00d
JackCeparou <JackCeparou@users.noreply.github.com>
2017-02-28 22:11:56 +0100

    faders

---
+++ Faders/AlwaysOnFader.cs
@@ -0,0 +1,12 @@
+﻿using Turbo.Plugins.Default;
+
+namespace Turbo.Plugins.Jack.Faders
+{
+    public class AlwaysOnFader : IFader
+    {
+        public bool TestVisiblity(bool visibleTestResult)
+        {
+            return true;
+        }
+    }
+}
---
+++ Faders/FakeFader.cs
@@ -0,0 +1,12 @@
+﻿using Turbo.Plugins.Default;
+
+namespace Turbo.Plugins.Jack.Faders
+{
+    public class FakeFader : IFader
+    {
+        public bool TestVisiblity(bool visibleTestResult)
+        {
+            return visibleTestResult;
+        }
+    }
+}
