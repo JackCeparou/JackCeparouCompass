41e6448c2af114636262f735538220e64c3707f5
JackCeparou <JackCeparou@users.noreply.github.com>
2017-03-05 19:47:18 +0100

    custom labels

--- Decorators/GroundCustomLabelDecorator.cs
+++ Decorators/GroundCustomLabelDecorator.cs
@@ -1,17 +1,46 @@
 using System.Collections.Generic;
 using System.Globalization;
 using Turbo.Plugins.Default;
-
+// this is an adaptation by Jack of a default hud helper, all credits to KillerJohn
 namespace Turbo.Plugins.Jack.Decorators
 {
 
     // this is not a plugin, just a helper class to display labels on the ground
-    public class GroundCustomLabelDecorator: GroundLabelDecorator
+    public class GroundCustomLabelDecorator: IWorldDecorator
     {
         public StringGeneratorFunc TextFunc { get; set; }
 
-        public GroundCustomLabelDecorator(IController hud) : base(hud)
+        public bool Enabled { get; set; }
+        public WorldLayer Layer { get; private set; }
+        public IController Hud { get; set; }
+
+        public IFont TextFont { get; set; }
+
+        // Option #1: Use brushes for background and border
+        public IBrush BackgroundBrush { get; set; }
+        public IBrush BorderBrush { get; set; }
+
+        // Option #2: Use textures for background
+        public ITexture BackgroundTexture1 { get; set; }
+        public ITexture BackgroundTexture2 { get; set; }
+        public float BackgroundTextureOpacity1 { get; set; }
+        public float BackgroundTextureOpacity2 { get; set; }
+
+        public int CountDownFrom { get; set; }
+
+        public float OffsetX { get; set; }
+        public float OffsetY { get; set; }
+        public bool CenterBaseLine { get; set; }
+        public bool ForceOnScreen { get; set; }
+
+        public GroundCustomLabelDecorator(IController hud)
         {
+            Enabled = true;
+            Layer = WorldLayer.Ground;
+            Hud = hud;
+
+            CenterBaseLine = true;
+            ForceOnScreen = true;
         }
 
         public GroundCustomLabelDecorator(IController hud, string text) : this(hud)
@@ -24,12 +53,12 @@ namespace Turbo.Plugins.Jack.Decorators
             TextFunc = textFunc;
         }
 
-        public new void Paint(IActor actor, IWorldCoordinate coord, string text)
+        public void Paint(IActor actor, IWorldCoordinate coord, string text)
         {
             if (!Enabled) return;
             if (TextFunc != null) text = TextFunc.Invoke();
 
-            var painter = Hud.GetPlugin<GroundLabelDecoratorPainterPlugin>();
+            var painter = Hud.GetPlugin<GroundCustomLabelDecoratorPainterPlugin>();
             if (painter == null) return;
 
             if ((CountDownFrom > 0) && (actor != null))
@@ -42,6 +71,13 @@ namespace Turbo.Plugins.Jack.Decorators
             }
 
             painter.EnqueLabelForPaint(this, coord, text);
+        }
+
+        public IEnumerable<ITransparent> GetTransparents()
+        {
+            yield return BackgroundBrush;
+            yield return BorderBrush;
+            yield return TextFont;
         }
 
     }
