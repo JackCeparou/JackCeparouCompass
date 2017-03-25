a1dc60a98922573429ba47145665507dcbb28b3c
JackCeparou <JackCeparou@users.noreply.github.com>
2017-03-25 22:42:58 +0100

    world decorator

---
+++ Extensions/WorldDecoratorCollectionExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace Turbo.Plugins.Jack.Extensions
+{
+    using Turbo.Plugins.Default;
+
+    public static class WorldDecoratorCollectionExtensions
+    {
+        public static void Add(this WorldDecoratorCollection collection, params IWorldDecorator[] decorators)
+        {
+            collection.Decorators.AddRange(decorators);
+        }
+    }
+}
