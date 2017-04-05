390a551e379b5d2f75018263cadc54bbd74bf203
JackCeparou <JackCeparou@users.noreply.github.com>
2017-04-06 01:06:02 +0200

    config feet buffs

--- Customize/Default/BuffListsConfigurator.cs
+++ Customize/Default/BuffListsConfigurator.cs
@@ -14,8 +14,20 @@
                 plugin.BuffPainter.ShowTimeLeftNumbers = true;
                 plugin.BuffPainter.Opacity = 0.85f;
 
+                var rules = plugin.RuleCalculator.Rules;
+
                 // Iron Skin
-                plugin.RuleCalculator.Rules.Add(new BuffRule(291804) { IconIndex = null, MinimumIconCount = 1, ShowTimeLeft = true, IconSizeMultiplier = 1.0f, });
+                rules.Add(new BuffRule(291804) { IconIndex = null, MinimumIconCount = 1, ShowTimeLeft = true, IconSizeMultiplier = 1.0f, });
+
+                // Pylons
+                rules.Add(new BuffRule(263029) { IconIndex = null, MinimumIconCount = 1, ShowTimeLeft = true }); // Pylon, Conduit - Normal Rift
+                rules.Add(new BuffRule(403404) { IconIndex = null, MinimumIconCount = 1, ShowTimeLeft = true }); // Pylon, Conduit - Greater Rift
+                rules.Add(new BuffRule(262935) { IconIndex = null, MinimumIconCount = 1, ShowTimeLeft = true }); // Pylon, Power
+                rules.Add(new BuffRule(266258) { IconIndex = null, MinimumIconCount = 1, ShowTimeLeft = true }); // Pylon, Channeling
+                rules.Add(new BuffRule(266254) { IconIndex = null, MinimumIconCount = 1, ShowTimeLeft = true }); // Pylon, Shield
+
+                // Legendary power
+                rules.Add(new BuffRule(402458) { IconIndex = 1, MinimumIconCount = 1, ShowTimeLeft = true }); // Legendary, In-Geom
             });
         }
     }
