6822af0101d292c87937350b06989b4e15615b55
JackCeparou <JackCeparou@users.noreply.github.com>
2017-04-06 19:32:27 +0200

    change debug text

--- DevTool/DisplayActorsPlugin.cs
+++ DevTool/DisplayActorsPlugin.cs
@@ -22,7 +22,7 @@ namespace Turbo.Plugins.Jack.DevTool
 
             Decorator = new GroundLabelDecorator(Hud)
             {
-                TextFont = Hud.Render.CreateFont("consolas", 10, 255, 255, 255, 255, false, false, 255, 0, 0, 0, true),
+                TextFont = Hud.Render.CreateFont("consolas", 8, 255, 255, 255, 255, false, false, 255, 0, 0, 0, true),
             };
         }
 
@@ -32,7 +32,14 @@ namespace Turbo.Plugins.Jack.DevTool
 
             foreach (var actor in Hud.Game.Actors)
             {
-                var text = string.Format("{0}\n{1}\n{2}", actor.SnoActor.Sno, actor.SnoActor.NameEnglish, actor.SnoActor.Code);
+                var text = string.Format("{0} : {1} {2}\n{3}, {4}, {5}\n{6}",
+                    actor.SnoActor.Sno,
+                    actor.SnoActor.NameLocalized,
+                    actor.SnoActor.Kind,
+                    actor.IsOperated ? "Operated" : "!Operated",
+                    actor.IsClickable ? "Clickable" : "!Clickable",
+                    actor.IsDisabled ? "Disabled" : "!Disabled",
+                    actor.SnoActor.Code);
                 Decorator.Paint(actor, actor.FloorCoordinate, text);
             }
         }
