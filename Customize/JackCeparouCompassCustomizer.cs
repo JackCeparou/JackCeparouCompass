11a7d2d77a4d274ab46b9232483da4ce56c4a58a
JackCeparou <JackCeparou@users.noreply.github.com>
2017-04-14 10:22:03 +0200

    config

--- Customize/JackCeparouCompassCustomizer.cs
+++ Customize/JackCeparouCompassCustomizer.cs
@@ -1,12 +1,7 @@
-using System;
-using Turbo.Plugins.Jack.Labs;
-using Turbo.Plugins.Jack.Players;
-
 namespace Turbo.Plugins.Jack.Customize
 {
     //using System.Collections.Generic;
     using Turbo.Plugins.Default;
-    using Turbo.Plugins.Jack.Actors;
     using Turbo.Plugins.Jack.Customize.JackCeparouCompass;
 
     public class JackCeparouCompassCustomizer : BasePlugin, ICustomizer
@@ -26,6 +21,12 @@ namespace Turbo.Plugins.Jack.Customize
         public void Customize()
         {
             //Says.Debug("J2 {0} {1}", "Customize", Order);
+
+            //Hud.RunOnPlugin<Gigi.PartyBuffPlugin>(plugin =>
+            //{
+            //    ISnoPower[] ev = Hud.Sno.AllSnoPower.ToArray();
+            //    plugin.DisplayOnAll(ev);
+            //});
 
             Hud.RunOnPlugin<Jack.Actors.DoorsPlugin>(plugin => plugin.ShowInTown = true);
             //Hud.RunOnPlugin<Jack.Actors.DoorsPlugin>(plugin =>
@@ -61,20 +62,49 @@ namespace Turbo.Plugins.Jack.Customize
                 //plugin.Clear();
             });
 
+            Hud.RunOnPlugin<Jack.Items.WeaponDamageRerollCalculatorPlugin>(plugin =>
+            {
+                plugin.RerollLabel = "Reroll";
+                plugin.DpsLabel = "Dps";
+                plugin.RangeLabel = "Range";
+                plugin.DamagePercentLabel = "Dmg %";
+                plugin.AttackSpeedLabel = "AS %";
+                plugin.PerfectLabel = "Perfect";
+                plugin.LineFormat = "{0}\t  {1}";
+                plugin.DpsFormat = "N1";
+                
+                plugin.NumberFormat = System.Globalization.NumberFormatInfo.InvariantInfo;
+                
+                plugin.ActiveFont = Hud.Render.CreateFont("tahoma", 7, 255, 154, 105, 24, true, false, 128, 0, 0, 0, true);
+                plugin.InactiveFont = Hud.Render.CreateFont("tahoma", 7, 128, 154, 105, 24, true, false, 128, 0, 0, 0, true);
+                plugin.RerollMaxedFont = Hud.Render.CreateFont("tahoma", 7, 255, 24, 192, 24, true, false, 128, 0, 0, 0, true);
+                
+                plugin.LeftFunc = () =>
+                {
+                    var uicMain = Hud.Inventory.GetHoveredItemMainUiElement();
+                    return uicMain.Rectangle.X + uicMain.Rectangle.Width * 0.69f;
+                };
+                plugin.TopFunc = () =>
+                {
+                    var uicTop = Hud.Inventory.GetHoveredItemTopUiElement();
+                    return uicTop.Rectangle.Bottom + (75f / 1200.0f * Hud.Window.Size.Height);
+                };
+            });
+
             ///////////
             // ITEMS //
             ///////////
-            Hud.RunOnPlugin<DefaultOverride.Items.ItemsPlugin>(plugin =>
-            {
-                //itemsPlugin.NormalKeepDecorator.Enabled = true;
-                //itemsPlugin.MagicKeepDecorator.Enabled = true;
-                //itemsPlugin.RareKeepDecorator.Enabled = true;
-                plugin.DeathsBreathDecorator.Add(new GroundCircleDecorator(Hud)
-                {
-                    Brush = Hud.Render.CreateBrush(192, 102, 202, 177, -2),
-                    Radius = 1.25f,
-                });
-            });
+            //Hud.RunOnPlugin<DefaultOverride.Items.ItemsPlugin>(plugin =>
+            //{
+            //    //itemsPlugin.NormalKeepDecorator.Enabled = true;
+            //    //itemsPlugin.MagicKeepDecorator.Enabled = true;
+            //    //itemsPlugin.RareKeepDecorator.Enabled = true;
+            //    plugin.DeathsBreathDecorator.Add(new GroundCircleDecorator(Hud)
+            //    {
+            //        Brush = Hud.Render.CreateBrush(192, 102, 202, 177, -2),
+            //        Radius = 1.25f,
+            //    });
+            //});
 
             //Hud.RunOnPlugin<Grischu.BuffStatistic.BuffStatistics>(plugin =>
             //{
