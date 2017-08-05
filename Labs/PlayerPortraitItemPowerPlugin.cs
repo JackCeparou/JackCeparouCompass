5fe4b981ad1658e1f2b155cbff3c164a75973993
JackCeparou <JackCeparou@users.noreply.github.com>
2017-08-05 20:48:51 +0200

    player portrait powers

---
+++ Labs/PlayerPortraitItemPowerPlugin.cs
@@ -0,0 +1,45 @@
+﻿using System.Drawing;
+using Turbo.Plugins;
+using Turbo.Plugins.Default;
+
+namespace Turbo.Plugins.Jack.Labs
+{
+    public class PlayerPortraitItemPowerPlugin : BasePlugin, IInGameTopPainter
+    {
+        private IBrush bgBrush;
+
+        public PlayerPortraitItemPowerPlugin()
+        {
+            Enabled = true;
+        }
+
+        public override void Load(IController hud)
+        {
+            base.Load(hud);
+
+            bgBrush = Hud.Render.CreateBrush(255, 0, 0, 0, 0);
+        }
+
+        public void PaintTopInGame(ClipState clipState)
+        {
+            if (clipState != ClipState.BeforeClip) return;
+
+            foreach (var player in Hud.Game.Players)
+            {
+                if (!player.Powers.BuffIsActive(Hud.Sno.SnoPowers.NemesisBracers.Sno)) continue;
+
+                var item = Hud.Sno.SnoItems.Unique_Bracer_106_x1;
+                var tex = Hud.Texture.GetItemTexture(item);
+                var bgTex = Hud.Texture.GetTexture(3166997520);
+
+                var portrait = player.PortraitUiElement.Rectangle;
+                var width = portrait.Left * 1.2f;
+                var rect = new RectangleF(0, portrait.Top, width, width * 2);
+
+                bgBrush.DrawRectangle(rect);
+                bgTex.Draw(rect.Left, rect.Top, rect.Width, rect.Height);
+                tex.Draw(rect.Left, rect.Top, rect.Width, rect.Height);
+            }
+        }
+    }
+}
