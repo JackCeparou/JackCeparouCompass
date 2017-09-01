4206e93db9460b9853945d8df741e3d78aff020c
JackCeparou <JackCeparou@users.noreply.github.com>
2017-09-01 03:19:53 +0200

    screen resize event experiment

---
+++ EventManager/ScreenResizeEventPlugin.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Turbo.Plugins.Jack.Interfaces;
+using Turbo.Plugins;
+using Turbo.Plugins.Default;
+
+namespace Turbo.Plugins.Jack.EventManager
+{
+    public class ScreenResizeEventPlugin : BasePlugin, IInGameTopPainter, INewAreaHandler
+    {
+        private int screenHeight;
+        private int screenWidth;
+
+        private static readonly List<IScreenResizeHandler> registredPlugins = new List<IScreenResizeHandler>();
+        public List<IScreenResizeHandler> RegistredPlugins
+        {
+            get { return registredPlugins; }
+        }
+
+        public ScreenResizeEventPlugin()
+        {
+            Enabled = true;
+            Order = int.MinValue;
+        }
+
+        public void OnNewArea(bool newGame, ISnoArea area)
+        {
+            if (newGame)
+                CheckSize();
+        }
+
+        public void PaintTopInGame(ClipState clipState)
+        {
+            if (clipState != ClipState.BeforeClip) return;
+
+            CheckSize();
+        }
+
+        public static void Register(IScreenResizeHandler plugin)
+        {
+            registredPlugins.Add(plugin);
+        }
+
+        private void CheckSize()
+        {
+            var size = Hud.Window.Size;
+            if (screenHeight == size.Height && screenWidth == size.Width) return;
+
+            foreach (var plugin in registredPlugins)
+            {
+                plugin.OnScreenResize(screenHeight, screenWidth, size.Height, size.Width);
+            }
+
+            screenHeight = size.Height;
+            screenWidth = size.Width;
+        }
+    }
+}
---
+++ Interfaces/IScreenResizeHandler.cs
@@ -0,0 +1,9 @@
+﻿using Turbo.Plugins;
+
+namespace Turbo.Plugins.Jack.Interfaces
+{
+    public interface IScreenResizeHandler : IPlugin
+    {
+        void OnScreenResize(float previousHeight, float previousWidth, float newHeight, float newWidth);
+    }
+}
