3500f63149d3c4e3729bf2302cfb70f15b252a2d
JackCeparou <JackCeparou@users.noreply.github.com>
2018-01-14 20:45:39 +0100

    highlight items from an armory set

--- Inventory/InventoryEquippedArmorySetPlugin.cs
+++ Inventory/InventoryEquippedArmorySetPlugin.cs
@@ -2,17 +2,25 @@
 {
     using Plugins;
     using Plugins.Default;
+    using SharpDX.Direct2D1;
+    using System;
+    using System.Collections.Generic;
     using System.Linq;
 
-    public class InventoryEquippedArmorySetPlugin : BasePlugin, IInGameTopPainter
+    public class InventoryEquippedArmorySetPlugin : BasePlugin, IInGameTopPainter, IItemLocationChangedHandler, INewAreaHandler
     {
-        public IFont ArmorySetFont { get; set; }
         public bool FirstSetOnly { get; set; }
+        public IFont ArmorySetFont { get; set; }
+        public IBrush ArmorySetBrush { get; set; }
+        public Action<IController, IItem, System.Drawing.RectangleF> DrawArmorySet { get; set; }
+
+        private HashSet<uint> equippedItemsAnnIdsHash;
 
         public InventoryEquippedArmorySetPlugin()
         {
             Enabled = true;
             FirstSetOnly = true;
+            DrawArmorySet = DrawArmorySetInternal;
         }
 
         public override void Load(IController hud)
@@ -20,6 +28,23 @@
             base.Load(hud);
 
             ArmorySetFont = Hud.Render.CreateFont("tahoma", 10, 255, 255, 163, 0, false, false, false);
+            ArmorySetBrush = Hud.Render.CreateBrush(255, 119, 171, 255, 1, DashStyle.Dash);
+        }
+
+        public void OnNewArea(bool newGame, ISnoArea area)
+        {
+            if (!newGame) return;
+
+            StateGuard(true);
+        }
+
+        public void OnItemLocationChanged(IItem item, ItemLocation @from, ItemLocation @to)
+        {
+            // if moved item is equipped/removed
+            if ((@from >= ItemLocation.Head && @from <= ItemLocation.Neck) || (@to >= ItemLocation.Head && @to <= ItemLocation.Neck))
+            {
+                StateGuard(true);
+            }
         }
 
         public void PaintTopInGame(ClipState clipState)
@@ -29,8 +54,54 @@
             var uiInv = Hud.Inventory.InventoryMainUiElement;
             if (!uiInv.Visible) return;
 
-            var annIds = Hud.Game.Items.Where(i => i.Location >= ItemLocation.Head && i.Location <= ItemLocation.Neck).Select(i => i.AnnId).ToList();
-            var setNames = Hud.Game.Me.ArmorySets.Where(set => set.ItemAnnIds.Any() && set.ItemAnnIds.All(id => annIds.Contains(id))).Select(set => set.Name).ToList();
+            StateGuard();
+            DrawEquippedSetName(uiInv, equippedItemsAnnIdsHash);
+
+            var stashTabAbs = Hud.Inventory.SelectedStashTabIndex + Hud.Inventory.SelectedStashPageIndex * Hud.Inventory.MaxStashTabCountPerPage;
+            var inventoryAndStashItems = Hud.Game.Items
+                .Where(item => item.Location == ItemLocation.Inventory || item.Location == ItemLocation.Stash)
+                .Where(item => item.Location != ItemLocation.Stash || (item.InventoryY / 10) == stashTabAbs)
+                .Where(item => item.InventoryX >= 0 && item.InventoryY >= 0)
+                ;
+            var visibleSetItems = inventoryAndStashItems
+                .Where(item => Hud.Game.Me.ArmorySets.Any(armorySet => armorySet.ContainsItem(item)));
+            foreach (var item in visibleSetItems)
+            {
+                DrawItem(item);
+            }
+        }
+
+        private void DrawItem(IItem item)
+        {
+            var rect = Hud.Inventory.GetItemRect(item);
+            if (rect == System.Drawing.RectangleF.Empty) return;
+
+            DrawArmorySet(Hud, item, rect);
+        }
+
+        private void StateGuard(bool resetState = false)
+        {
+            if (resetState)
+            {
+                equippedItemsAnnIdsHash = null;
+                return;
+            }
+
+            if (equippedItemsAnnIdsHash != null) return;
+
+            var equippedItemsAnnIds = Hud.Game.Items
+                    .Where(i => i.Location >= ItemLocation.Head && i.Location <= ItemLocation.Neck)
+                    .Select(i => i.AnnId)
+                    .ToList();
+            equippedItemsAnnIdsHash = new HashSet<uint>(equippedItemsAnnIds);
+        }
+
+        private void DrawEquippedSetName(IUiElement uiInv, HashSet<uint> annIdsHash)
+        {
+            var setNames =
+                Hud.Game.Me.ArmorySets.Where(set => set.ItemAnnIds.Any() && set.ItemAnnIds.All(id => annIdsHash.Contains(id)))
+                    .Select(set => set.Name)
+                    .ToList();
 
             if (setNames.Count == 0) return;
 
@@ -41,6 +112,11 @@
             var y = uiInv.Rectangle.Top + (uiInv.Rectangle.Width * 0.21f) - layout.Metrics.Height;
 
             ArmorySetFont.DrawText(layout, x, y);
+        }
+
+        private void DrawArmorySetInternal(IController hud, IItem item, System.Drawing.RectangleF rect)
+        {
+            ArmorySetBrush.DrawRectangle(rect);
         }
     }
 }
