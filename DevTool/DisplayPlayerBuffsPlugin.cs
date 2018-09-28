99872058bd91b742f360d53afe6755723431384e
JackCeparou <JackCeparou@users.noreply.github.com>
2018-09-28 22:40:37 +0200

    update DisplayPlayerBuffsPlugin layout and display only used icons

--- DevTool/DisplayPlayerBuffsPlugin.cs
+++ DevTool/DisplayPlayerBuffsPlugin.cs
@@ -20,15 +20,17 @@ namespace Turbo.Plugins.Jack.DevTool
         {
             Enabled = true;
             StartX = 20f;
-            StartY = 50f;
+            StartY = 20f;
         }
 
         public override void Load(IController hud)
         {
             base.Load(hud);
 
-            Font = Hud.Render.CreateFont("tahoma", 7, 255, 0, 170, 0, false, false, true);
-            BackgroundBrush = Hud.Render.CreateBrush(142, 92, 92, 92, 0);
+            //_showList = true;
+
+            Font = Hud.Render.CreateFont("consolas", 6, 255, 0, 170, 0, false, false, true);
+            BackgroundBrush = Hud.Render.CreateBrush(192, 92, 92, 92, 0);
 
             DisplayHotKey = Hud.Input.CreateKeyEvent(true, SharpDX.DirectInput.Key.B, true, false, false);
             DumpHotKey = Hud.Input.CreateKeyEvent(true, SharpDX.DirectInput.Key.B, true, false, true);
@@ -39,6 +41,7 @@ namespace Turbo.Plugins.Jack.DevTool
             if (keyEvent.Matches(DumpHotKey) && keyEvent.IsPressed)
             {
                 DumpToFile();
+                return;
             }
             if (keyEvent.Matches(DisplayHotKey) && keyEvent.IsPressed)
             {
@@ -59,22 +62,48 @@ namespace Turbo.Plugins.Jack.DevTool
             if (!_showList) return;
 
             var lines = GetBuffLines();
-            var lineWithMaxLength = lines.Where(l => l.Length == lines.Max(x => x.Length)).FirstOrDefault();
+            var lineWithMaxLength = lines.FirstOrDefault(l => l.Length == lines.Max(ll => ll.Length));
 
             if (string.IsNullOrEmpty(lineWithMaxLength)) return;
 
             var layout = Font.GetTextLayout(lineWithMaxLength);
             var h = layout.Metrics.Height;
             var w = layout.Metrics.Width;
+            var x = StartX;
             var y = StartY;
 
+            //var lineCount = (int)((Hud.Window.GroundRectangle.Top + Hud.Window.GroundRectangle.Height - StartX) / h);
+            //lineCount /= 2;
+            //var columns = new List<List<string>>()
+            //{
+            //    new List<string>()
+            //};
+            //var currentColumnIndex = 0;
+            //while (currentColumnIndex * lineCount < lines.Count)
+            //{
+            //    var column = lines.Skip(currentColumnIndex * lineCount).Take(lineCount);
+            //    columns[currentColumnIndex].AddRange(column);
+            //    currentColumnIndex++;
+            //}
+
+            var lineCount = (int)((Hud.Window.GroundRectangle.Top + Hud.Window.GroundRectangle.Height - StartX) / h);
+            var estimatedWidth = w * ((lines.Count / lineCount) + 1);
             if (BackgroundBrush != null)
-                BackgroundBrush.DrawRectangle(StartX - h / 2f, StartY - h / 2f, w + h, h * lines.Count + h);
+                BackgroundBrush.DrawRectangle(StartX - h / 2f, StartY - h / 2f, estimatedWidth + h, h * Math.Min(lineCount, lines.Count) + h);
 
             foreach (var line in GetBuffLines())
             {
-                Font.DrawText(line, StartX, y);
-                y += h;
+                Font.DrawText(line, x, y);
+
+                if (y >= Hud.Window.GroundRectangle.Bottom - (h*2))
+                {
+                    x += w;
+                    y = StartY;
+                }
+                else
+                {
+                    y += h;
+                }
             }
         }
 
@@ -84,20 +113,47 @@ namespace Turbo.Plugins.Jack.DevTool
                     .Where(buff => buff != null && buff.SnoPower != null)
                     .OrderBy(buff => string.IsNullOrEmpty(buff.SnoPower.NameEnglish) ? 1 : 0)
                     .ThenBy(buff => buff.SnoPower.Sno)
-                    .Select(buff => string.Join(
-                        " | ",
-                        string.IsNullOrEmpty(buff.SnoPower.NameEnglish)
-                            ? buff.SnoPower.Sno.ToString()
-                            : string.Format("{0} | {1}", buff.SnoPower.NameEnglish, buff.SnoPower.Sno.ToString()),
-                        buff.SnoPower.Code,
-                        buff.Active,
-                        //string.Format("{0:0.#}", buff.TimeLeft()),
-                        string.Join(", ", buff.IconCounts.Select(i => string.Format("{0:0.#}", i))),
-                        string.Join(", ", buff.TimeLeftSeconds.Select(t => string.Format("{0:0.#}", t)))
-                        ))
-                        .ToList();
+                    .Select(buff =>
+                    {
+                        var iconCount = IconCount(buff.IconCounts);
+                        return string.Join(
+                            " | ",
+                            string.Format("{0} {1,6} {2}",
+                                buff.Active ? "⚫" : "⚪",
+                                buff.SnoPower.Sno,
+                                string.IsNullOrEmpty(buff.SnoPower.NameEnglish)
+                                    ? buff.SnoPower.Code
+                                    : buff.SnoPower.NameEnglish
+                            ),
+                            iconCount,
+                            string.Join(" ",
+                                buff.IconCounts.Take(iconCount).Select(i => string.Format("{0:0.#}", i))),
+                            string.Join(" ",
+                                buff.TimeLeftSeconds.Take(iconCount)
+                                    .Select(t => string.Format("{0,3:0.0}", t)))
+                        );
+                    })
+                    .ToList();
 
             return lines;
+        }
+
+        private int IconCount(int[] items)
+        {
+            return LastIndexOf(items, 1) + 1;
+        }
+
+        private int LastIndexOf(int[] items, int value)
+        {
+            for (var index = items.Length-1; index > 0; index--)
+            {
+                if (items[index] < value)
+                    continue;
+
+                return index;
+            }
+
+            return 0;
         }
     }
 }
