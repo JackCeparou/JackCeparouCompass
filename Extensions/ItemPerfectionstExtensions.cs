0914237d41a544a82bf32694c086dfcb4b46b88e
JackCeparou <JackCeparou@users.noreply.github.com>
2019-02-21 21:32:59 +0100

    null check on attribute

--- Extensions/ItemPerfectionstExtensions.cs
+++ Extensions/ItemPerfectionstExtensions.cs
@@ -1,51 +1,64 @@
 ﻿namespace Turbo.Plugins.Jack.Extensions
 {
+    using System;
     using System.Collections.Generic;
     using System.Linq;
 
-    public static class ItemPerfectionstExtensions
+    public static class ItemPerfectionsExtensions
     {
+        public static int Get(this IEnumerable<IItemPerfection> perfections, string code, Func<IItemPerfection, double?> select)
+        {
+            return perfections
+                .Where(s => s?.Attribute?.Code == code)
+                .Select(s => (int)(select?.Invoke(s) ?? -1))
+                .FirstOrDefault();
+        }
+
         public static int WeaponDamageBaseMin(this IEnumerable<IItemPerfection> perfections)
         {
-            return perfections.Where(s => s.Attribute.Code == "Damage_Weapon_Min").Select(s => (int)s.Cur).FirstOrDefault();
+            return perfections.Get("Damage_Weapon_Min", s => s?.Cur);
         }
 
         public static int WeaponDamageBaseMax(this IEnumerable<IItemPerfection> perfections)
         {
-            return perfections.WeaponDamageBaseMin() + perfections.Where(s => s.Attribute.Code == "Damage_Weapon_Delta").Select(s => (int)s.Cur).FirstOrDefault();
+            var itemPerfections = perfections as IItemPerfection[] ?? perfections.ToArray();
+            return itemPerfections.WeaponDamageBaseMin() + itemPerfections.Get("Damage_Weapon_Delta", s => s?.Cur);
         }
 
         public static float WeaponDamageBonusMin(this IEnumerable<IItemPerfection> perfections)
         {
-            return perfections.Where(s => s.Attribute.Code == "Damage_Weapon_Min").Select(s => (int)s.Cur).FirstOrDefault();
+            return perfections.Get("Damage_Weapon_Min", s => s?.Cur);
         }
 
         public static float WeaponDamageBonusMax(this IEnumerable<IItemPerfection> perfections)
         {
-            return perfections.WeaponDamageBaseMin() + perfections.Where(s => s.Attribute.Code == "Damage_Weapon_Delta").Select(s => (int)s.Cur).FirstOrDefault();
+            var itemPerfections = perfections as IItemPerfection[] ?? perfections.ToArray();
+            return itemPerfections.WeaponDamageBaseMin() + itemPerfections.Get("Damage_Weapon_Delta", s => s?.Cur);
         }
 
         public static float WeaponDamageBonusMinMin(this IEnumerable<IItemPerfection> perfections)
         {
-            return perfections.Where(s => s.Attribute.Code == "Damage_Weapon_Min").Select(s => (int)s.Min).FirstOrDefault();
+            return perfections.Get("Damage_Weapon_Min", s => s?.Min);
         }
         public static float WeaponDamageBonusMinMax(this IEnumerable<IItemPerfection> perfections)
         {
-            return perfections.Where(s => s.Attribute.Code == "Damage_Weapon_Min").Select(s => (int)s.Max).FirstOrDefault();
+            return perfections.Get("Damage_Weapon_Min", s => s?.Max);
         }
 
         public static float WeaponDamageBonusMaxMin(this IEnumerable<IItemPerfection> perfections)
         {
-            return perfections.WeaponDamageBonusMinMin() + perfections.Where(s => s.Attribute.Code == "Damage_Weapon_Delta").Select(s => (int)s.Min).FirstOrDefault();
+            var itemPerfections = perfections as IItemPerfection[] ?? perfections.ToArray();
+            return itemPerfections.WeaponDamageBonusMinMin() + itemPerfections.Get("Damage_Weapon_Delta", s => s?.Min);
         }
         public static float WeaponDamageBonusMaxMax(this IEnumerable<IItemPerfection> perfections)
         {
-            return perfections.WeaponDamageBonusMaxMin() + perfections.Where(s => s.Attribute.Code == "Damage_Weapon_Delta").Select(s => (int)s.Max).FirstOrDefault();
+            var itemPerfections = perfections as IItemPerfection[] ?? perfections.ToArray();
+            return itemPerfections.WeaponDamageBonusMaxMin() + itemPerfections.Get("Damage_Weapon_Delta", s => s?.Max);
         }
 
         public static IItemPerfection Intelligence(this IEnumerable<IItemPerfection> perfections)
         {
-            return perfections.FirstOrDefault(p => p.Attribute.Code == "Intelligence_Item");
+            return perfections.FirstOrDefault(p => p?.Attribute?.Code == "Intelligence_Item");
         }
 
         public static double Percent(this IItemPerfection perfection)
