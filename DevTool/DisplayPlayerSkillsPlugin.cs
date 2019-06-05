3035b4f69d63f2bf9b23d26c2545dcffda432ce6
JackCeparou <JackCeparou@users.noreply.github.com>
2019-06-05 21:06:47 +0200

    DisplayPlayerSkillsPlugin

--- DevTool/DisplayPlayerSkillsPlugin.cs
+++ DevTool/DisplayPlayerSkillsPlugin.cs
@@ -5,7 +5,7 @@ using Turbo.Plugins.Default;
 
 namespace Turbo.Plugins.Jack.DevTool
 {
-    public class DisplayPlayerBuffsPlugin : BasePlugin, IInGameTopPainter, IKeyEventHandler
+    public class DisplayPlayerSkillsPlugin : BasePlugin, IInGameTopPainter, IKeyEventHandler
     {
         public IKeyEvent DisplayHotKey { get; set; }
         public IKeyEvent DumpHotKey { get; set; }
@@ -16,7 +16,7 @@ namespace Turbo.Plugins.Jack.DevTool
 
         private bool _showList;
 
-        public DisplayPlayerBuffsPlugin()
+        public DisplayPlayerSkillsPlugin()
         {
             Enabled = true;
             StartX = 20f;
@@ -32,8 +32,8 @@ namespace Turbo.Plugins.Jack.DevTool
             Font = Hud.Render.CreateFont("consolas", 6, 255, 0, 170, 0, false, false, true);
             BackgroundBrush = Hud.Render.CreateBrush(192, 92, 92, 92, 0);
 
-            DisplayHotKey = Hud.Input.CreateKeyEvent(true, SharpDX.DirectInput.Key.B, true, false, false);
-            DumpHotKey = Hud.Input.CreateKeyEvent(true, SharpDX.DirectInput.Key.B, true, false, true);
+            DisplayHotKey = Hud.Input.CreateKeyEvent(true, SharpDX.DirectInput.Key.N, true, false, false);
+            DumpHotKey = Hud.Input.CreateKeyEvent(true, SharpDX.DirectInput.Key.N, true, false, true);
         }
 
         public void OnKeyEvent(IKeyEvent keyEvent)
@@ -51,7 +51,7 @@ namespace Turbo.Plugins.Jack.DevTool
 
         private void DumpToFile()
         {
-            var fileName = string.Format("dump_player_buffs_{0:yyMMddHHmmss}.txt", DateTime.Now);
+            var fileName = string.Format("dump_player_skills_{0:yyMMddHHmmss}.txt", DateTime.Now);
             var text = string.Join(Environment.NewLine, GetBuffLines());
             Hud.TextLog.Log(fileName, text, false);
         }
@@ -109,31 +109,31 @@ namespace Turbo.Plugins.Jack.DevTool
 
         private List<string> GetBuffLines()
         {
-            var lines = Hud.Game.Me.Powers.AllBuffs
-                    .Where(buff => buff != null && buff.SnoPower != null)
-                    .OrderBy(buff => string.IsNullOrEmpty(buff.SnoPower.NameEnglish) ? 1 : 0)
-                    //.ThenBy(buff => IconCount(buff.IconCounts))
-                    .ThenBy(buff => buff.SnoPower.Sno)
-                    .Select(buff =>
+            var lines = Hud.Game.Me.Powers.CurrentSkills
+                    .Where(skill => skill != null && skill.SnoPower != null)
+                    .OrderBy(skill => string.IsNullOrEmpty(skill.SnoPower.NameEnglish) ? 1 : 0)
+                    .ThenBy(skill => skill.SnoPower.Sno)
+                    .Select(skill =>
                     {
-                        var iconCount = IconCount(buff.IconCounts);
+                        //var iconCount = IconCount(skill.Buff.IconCounts);
                         return string.Join(
                             " | ",
                             string.Format("{0} {1,6} {2}",
-                                buff.Active ? "⚫" : "⚪",
-                                buff.SnoPower.Sno,
-                                string.IsNullOrEmpty(buff.SnoPower.NameEnglish)
-                                    ? buff.SnoPower.Code
+                                skill.BuffIsActive ? "⚫" : "⚪",
+                                skill.SnoPower.Sno,
+                                string.IsNullOrEmpty(skill.SnoPower.NameEnglish)
+                                    ? skill.SnoPower.Code
                                         .Replace("ItemPassive_Unique", "*")
                                         .Replace("Community_Event_Buff", "*")
-                                    : buff.SnoPower.NameEnglish
+                                    : skill.SnoPower.NameEnglish
                             ),
-                            iconCount,
+                            //iconCount,
                             //string.Join(" ",
                             //    buff.IconCounts.Take(iconCount).Select(i => string.Format("{0:0.#}", i))),
-                            string.Join(" ",
-                                buff.TimeLeftSeconds.Take(iconCount)
-                                    .Select(t => string.Format("{0,3:0.0}", t)))
+                            //string.Join(" ",
+                            //    skill.Buff.TimeLeftSeconds.Take(iconCount)
+                            //        .Select(t => string.Format("{0,3:0.0}", t))),
+                            string.Format("{0} {1}", skill.Rune, skill.RuneNameEnglish)
                         );
                     })
                     .ToList();
